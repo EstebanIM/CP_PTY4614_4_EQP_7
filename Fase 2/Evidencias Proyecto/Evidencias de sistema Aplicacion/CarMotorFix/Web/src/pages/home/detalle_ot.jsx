bbb936babf84b129874a8f02b4a986c4f2f1d88c
Diego Alonso <21320532+Diego0Alonso@users.noreply.github.com>
2024-11-14 18:39:32 -0300

    Boton Aceptado y Rechazado, para detalle OT. con envio de correo

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/CarMotorFix/Web/src/pages/home/detalle_ot.jsx
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/CarMotorFix/Web/src/pages/home/detalle_ot.jsx
@@ -24,8 +24,13 @@ export default function WorkOrderDetails() {
   const [userRole, setUserRole] = useState(null);
   const [showAddEstado, setshowAddEstado] = useState(false);
   const [newEstado, setNewEstado] = useState([]);
-  const [someValue, setSomeValue] = useState('');
 
+  const [formData, setFormData] = useState({
+    descripcion: '',
+    fecharecepcion: '',
+    fechaentrega: '',
+    catalogo_servicios: []
+  });
 
   const STRAPI_URL = import.meta.env.VITE_STRAPI_URL;
 
@@ -83,7 +88,7 @@ export default function WorkOrderDetails() {
 
           setVehiculo(response.data);
 
-          console.log(response.data);
+          // console.log(response.data);
 
 
         } catch (error) {
@@ -111,7 +116,6 @@ export default function WorkOrderDetails() {
       }
     }
   };
-
 
   useEffect(() => {
     setLoading(true);
@@ -158,6 +162,155 @@ export default function WorkOrderDetails() {
     return <Loading />;
   }
 
+  const handleSubmitEstado = async (e) => {
+    e.preventDefault();
+
+    const jwt = getTokenFromLocalCookie();
+    if (jwt) {
+      try {
+
+        // setLoading(true);
+        // const vehiculoData = {
+        //   data: {
+        //     user_id: Cookies.get('id'),
+        //     marca_id: newVehiculo.marca_id,
+        //     tp_vehiculo_id: newVehiculo.tp_vehiculo_id,
+        //     modelo: newVehiculo.modelo,
+        //     patente: newVehiculo.patente,
+        //     anio: newVehiculo.anio,
+        //     kilometraje: Number(newVehiculo.kilometraje),
+        //     motor: newVehiculo.motor,
+        //     color: newVehiculo.color
+        //   }
+        // };
+
+        // const response = await fetcher(`${STRAPI_URL}/api/vehiculos`, {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //     Authorization: `Bearer ${jwt}`,
+        //   },
+        //   body: JSON.stringify(vehiculoData),
+        // });
+
+        // setVehiculos([...vehiculos, response.data]);
+        // setNewVehiculo({
+        //   patente: '',
+        //   anio: '',
+        //   kilometraje: '',
+        //   modelo: '',
+        //   motor: '',
+        //   color: '',
+        //   marca_id: '',
+        //   tp_vehiculo_id: ''
+        // });
+        // setShowAddVehiculoModal(false);
+        // toast.success("Vehículo agregado correctamente");
+      } catch (error) {
+        console.error('Error adding vehicle:', error);
+        // toast.error("Error al agregar el vehículo");
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  const actualizarEstadoOrden = async (id, nuevoEstado) => {
+    const jwt = getTokenFromLocalCookie();
+    try {
+      const updateData = {
+        data: {
+          estado_ot_id: nuevoEstado,
+        },
+      };
+  
+      // Actualizamos el estado de la orden en Strapi
+      const response = await fetch(`${STRAPI_URL}/api/orden-trabajos/${id}`, { 
+        method: 'PUT', 
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: JSON.stringify(updateData), 
+      });
+  
+      // console.log('response:', response);
+      // console.log('updateData:', updateData);
+  
+      if (!response.ok) {
+        throw new Error(`Error al actualizar la orden: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log('Orden actualizada exitosamente:', data);
+  
+      // const { clienteEmail, mecanicoEmail } = data; // Asegúrate de que esta respuesta contiene los emails correctos
+      
+      const clienteEmail = Orden.user.email;
+      const mecanicoEmail = Orden.mecanico_id.correo;
+
+      // Enviar email al cliente
+      const clienteEmailData = {
+        to: clienteEmail,
+        subject: "Actualización de Estado de su Orden de Trabajo",
+        html: `
+        <p>El estado de su orden de trabajo ha sido actualizado.</p>
+        <p>El estado de su orden de trabajo ha sido actualizado a: ${nuevoEstado === 2 ? 'Aceptado' : 'Rechazado'}</p>`,
+      };
+  
+      const clienteEmailResponse = await fetch(`http://localhost:1337/api/email/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: JSON.stringify(clienteEmailData),
+      });
+  
+      if (!clienteEmailResponse.ok) {
+        throw new Error(`Error al enviar el email al cliente: ${clienteEmailResponse.statusText}`);
+      }
+  
+      console.log('Email enviado exitosamente al cliente');
+  
+      // Enviar email al mecánico
+      const mecanicoEmailData = {
+        to: mecanicoEmail,
+        subject: "Notificación de Actualización de Orden de Trabajo",
+        html: `
+        <p>El estado de la orden de trabajo que estás gestionando ha sido actualizado.</p>
+        <p>El estado de la orden de trabajo ha sido actualizado a: ${nuevoEstado === 2 ? 'Aceptado' : 'Rechazado'}</p>`,
+      };
+  
+      const mecanicoEmailResponse = await fetch(`http://localhost:1337/api/email/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: JSON.stringify(mecanicoEmailData),
+      });
+  
+      if (!mecanicoEmailResponse.ok) {
+        throw new Error(`Error al enviar el email al mecánico: ${mecanicoEmailResponse.statusText}`);
+      }
+  
+      console.log('Email enviado exitosamente al mecánico');
+  
+      // Actualizamos el estado local de la orden
+      setOrden((prevOrden) => ({
+        ...prevOrden,
+        estado_ot_id: { nom_estado: nuevoEstado === 2 ? 'Aceptado' : 'Rechazado' },
+      }));
+  
+      return data;
+    } catch (error) {
+      console.error('Error en la actualización de la orden o envío de email:', error);
+    }
+  };
+  
+
+
   return (
     <div className={`flex h-screen ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
       <DashboardSidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} userRole={userRole} />
@@ -170,7 +323,28 @@ export default function WorkOrderDetails() {
           </div>
 
           <Card className="p-6">
-            <h2 className="text-2xl font-bold mb-6">Orden #{Orden.id}</h2>
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-2xl font-bold">Orden #{Orden.id}</h2>
+              {Orden.descripcion &&
+                userRole === 'Authenticated' &&
+                Orden.estado_ot_id?.nom_estado !== 'Aceptado' &&
+                Orden.estado_ot_id?.nom_estado !== 'Rechazado' && (
+                  <div className="space-x-4">
+                    <button className="px-4 py-2 bg-green-700 text-white rounded"
+                    onClick={() => actualizarEstadoOrden(Orden.documentId, 2)} // 2 para "Aceptado"
+                    >
+                      Aceptar
+                    </button>
+                    <button className="px-4 py-2 bg-red-700 text-white rounded"
+                    onClick={() => actualizarEstadoOrden(Orden.documentId, 4)} // 4 para "Rechazado"
+                    >
+                      Rechazar
+                    </button>
+                  </div>
+                )}
+
+            </div>
+
             <div className="grid grid-cols-2 gap-4 mb-6">
               <div>
                 <div className="text-sm text-muted-foreground">Cliente</div>
@@ -214,6 +388,12 @@ export default function WorkOrderDetails() {
                   {Orden.costo !== undefined && Orden.costo !== null ? Orden.costo.toLocaleString('es-CL', { style: 'currency', currency: 'CLP' }) : 'No disponible'}
                 </div>
               </div>
+              {Orden.descripcion && (
+                <div>
+                  <div className="text-sm text-muted-foreground">Descripción</div>
+                  <div className="font-medium">{Orden.descripcion}</div>
+                </div>
+              )}
             </div>
 
             <div className="space-y-4">
@@ -248,14 +428,14 @@ export default function WorkOrderDetails() {
 
           <Modal isOpen={showAddEstado} onClose={() => setshowAddEstado(false)}>
             <h4 className="text-xl font-semibold mb-4">Actualizar Orden</h4>
-            <form>
+            <form onSubmit={handleSubmitEstado}>
               <div className="grid gap-4">
                 <label htmlFor="estado" className="text-sm font-medium">Estado</label>
                 <select
                   id="estado"
                   name="estado"
-                  value={someValue}
-                  onChange={(e) => setSomeValue(e.target.value)}
+                  value=""
+                  onChange=""
                   required
                   className="p-2 border rounded"
                 >
@@ -266,27 +446,31 @@ export default function WorkOrderDetails() {
                       <option key={tipo.id} value={tipo.id}>{tipo.nom_estado}</option>
                     ))}
                 </select>
+
                 <label htmlFor="fecha" className="text-sm font-medium">Fecha</label>
                 <input
                   type="date"
                   id="fecha"
                   name="fecha"
-                  value={someValue}
-                  onChange={(e) => setSomeValue(e.target.value)}
+                  value=""
+                  onChange=""
                   required
                   className="p-2 border rounded"
                 />
-                {someValue !== 'Nueva Cotización' && (
+
+                {/* {estado === 'Nueva Cotización' && (
                   <>
                     <label htmlFor="descripcion" className="text-sm font-medium">Descripción</label>
                     <textarea
                       id="descripcion"
                       name="descripcion"
                       placeholder="Ingrese descripción"
+                      value={descripcion}
+                      onChange={(e) => setDescripcion(e.target.value)}
                       className="p-2 border rounded"
                     />
                   </>
-                )}
+                )} */}
               </div>
               <button type="submit" className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500">
                 Actualizar
