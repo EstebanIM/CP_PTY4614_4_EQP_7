346f595fabdf2a107a8040a42369626298fd883d
Diego Alonso <21320532+Diego0Alonso@users.noreply.github.com>
2024-11-26 16:06:33 -0300

    Corrección error con strApi

--- Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/CarMotorFix/Web/src/lib/strApi.js
+++ Fase 2/Evidencias Proyecto/Evidencias de sistema Aplicacion/CarMotorFix/Web/src/lib/strApi.js
@@ -1,20 +1,5 @@
 export async function fetcher(url, options = {}) {
-    const token = localStorage.getItem('jwt'); // Recuperar el token del almacenamiento local
-  
-    // Añadir el token a las cabeceras si existe
-    const headers = {
-      'Content-Type': 'application/json',
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
-      ...options.headers,
-    };
-  
-    const response = await fetch(url, { ...options, headers });
-    
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
-    }
-  
-    const data = await response.json();
-    return data;
-  }
-  
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
